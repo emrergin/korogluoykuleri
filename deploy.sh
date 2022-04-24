fedec14df08cb629bc99a9fd7ffde0b77b9c5c34
emrergin <emrergin2757@yahoo.com>
2022-04-25 00:15:08 +0300

    yeni hikayeler

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+node oykuleriYenile.js
+git add -A
+git commit -m 'yeni hikayeler'
+git push main
+
+cd -
+
+
+
+#!/usr/bin/env sh
+
+# abort on errors
+# set -e
+
+# build
+# npm run build
+
+# navigate into the build output directory
+# cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+# git init
+# git checkout -b main
